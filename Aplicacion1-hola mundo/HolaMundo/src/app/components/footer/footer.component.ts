c49726d87b284c6122e16c0acea0f990dd350d16
jorge lopez <jorge.alpcu@gmail.com>
2019-02-01 23:25:26 -0600

    actualizacion 3 apk-holamundo
    agregamos el footer

---
+++ Aplicacion1-hola mundo/HolaMundo/src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent implements OnInit {
+ano: number;
+  constructor() {
+    this.ano= new Date().getFullYear();
+   }
+
+  ngOnInit() {
+  }
+
+}
